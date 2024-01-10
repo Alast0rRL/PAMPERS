a139d3ad053966698e26e0e941a0db6c4736ee54
Parseltang <eshenkove@gmail.com>
2024-01-10 23:50:32 +0500

    beta 2.0

---
+++ config.js
@@ -0,0 +1,4 @@
+config = {
+        'api_key': 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,
+        'proxy': прокси
+}
---
+++ gpt test.py
@@ -0,0 +1,26 @@
+import os
+import openai
+openai.api_key = os.getenv("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")
+
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+  ]
+)
+
+print(completion.choices[0].message.content)
+import openai
+
+while True:
+    content = input("User: ")
+    messages.append({"role": "user", "content": content})
+    
+    completion = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+
+    chat_response = completion.choices[0].message.content
+    print(f'ChatGPT: {chat_response}')
+    messages.append({"role": "assistant", "content": chat_response})
